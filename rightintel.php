548c373c079c7ffd8f40a72423dd98450d2a3e0e
John D. Storey <johndangerstorey@gmail.com>
2016-02-20 00:08:38 -0700

    initial commit with legacy code

---
+++ rightintel.php
@@ -0,0 +1,113 @@
+<?php include 'header.html'; ?>
+    <!-- masthead -->
+    <div class="jumbotron">
+      <h2 class="jumbotron--title">Right Intel</h2>
+      <p class="jumbotron--sub-title">RESPONSIVE <span class="icon-star"></span> WORDPRESS</p>
+      <a href="http://www.rightintel.com" class="btn  btn--green">See Site</a>
+    </div><!-- /masthead -->
+
+        <!-- main-content -->
+        <div class="container  main-content">
+
+          <div class="row">
+
+            <!-- left content -->
+            <div class="col-md-8  blog">
+
+              <!-- blog post -->
+              <img src="img/rightintel.png" class="blog-main-img  img-responsive" alt="blog main image" />
+
+              <h4 class="blog-post-title">Freelance</h4>  
+
+              <h6 class="blog-post-sub-title">
+              While attending Dev Mountain we had a presenter come talk about their startup called Right Intel.  They mentioned they were in need of a developer to convert their normal site into a responsive one.  I had started using bootstrap and was familiar with thier grid system so I quickly jumped on board working with their designer.
+              </h6>  
+                <ul class="list-unstyled  tags  blog-tags">
+                  <li><a>What I've done since</a></li>
+                </ul>
+              <h6 class="blog-post-sub-title">
+              	The position has had me implement google analytics, A/B split testing, and form submital through PHP.  Building {shortcode} templates has been my most enjoyable project, as well as becoming better aquainted with GitHub and how to work with a development team that is already setup and going.  Also writing javascript to fade in and out the background images and auto-download PDF files after form submital. 
+              </h6>
+
+            </div><!-- /left content -->
+
+              <!-- right content -->
+              <div class="col-md-4  right-hand-bar">
+
+                <hr class="hidden-lg">
+
+                  <h5 class='no-top-margin'>Noteable</h5>
+
+                  <ul class="list-unstyled  tags  category-tags">
+                    <li><a>PHP Introduction</a></li>
+                    <li><a>Wordpress Development</a></li>
+                    <li><a>Work with designer</a></li>
+                    <li><a>Pixel Perfect</a></li>
+                    <li><a>Deadlines Met</a></li>
+                    <li><a>Responsive Migration</a></li>
+                    <li><a>HTML</a></li>
+                    <li><a>CSS</a></li>
+                    <li><a>Javascript</a></li>
+                  </ul>
+
+	 			<div class="col-md-12 sidebarPictures no-padding">
+	 			<img src="img/riWebcast.png" class="img-responsive" alt="webcast landing page" />
+              	<img src="img/riMarketers.png" class="img-responsive" alt="webcast landing page" />
+				</div>
+
+              </div><!-- /right content -->
+
+          </div>
+
+        </div><!-- /main-content -->
+
+    <!-- footer -->
+    <section class="footer  flush--top">
+      <div class="container">
+        <div class="row">
+          <ul class="list-inline  col-12  col-sm-3  col-lg-2">
+            <li><a href="mailto:johndangerstorey@gmail.com">Email</a></li>
+            <li><a href="https://www.facebook.com/johndangerstorey" target="_blank">Facebook</a></li>
+            <li><a href="https://twitter.com/johnDANGRstorey" target="_blank">Tweet</a></li> 
+            <li><a href="John_Design&Dev.pdf" target='_blank'>Resume</a></li>
+          </ul>
+<!--           <ul class="list-unstyled  col-12  col-sm-2  col-lg-2">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="work.html">Work</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="shortcodes.html">Shortcodes</a></li>
+            <li><a href="archives.html">Archives</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul> -->
+          <div class="col-12  col-sm-7  col-lg-5  col-lg-offset-3  text-right  go-right">
+            <h3 class="footer--main-logo">d.</h3>
+            <p>
+              The d. Stands For Danger
+            </p>
+          </div>
+        </div>
+      </div>
+    </section><!-- footer -->
+
+    <!-- jQuery -->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- touch effects for hover effects in portfolio section -->
+    <script src="js/toucheffects.min.js"></script>
+    <script src="js/classie.min.js"></script> 
+
+    <!-- bxSlider js file -->
+    <script src="js/jquery.bxslider.js"></script>
+
+    <!-- Envy js -->
+    <script src="js/envy.js"></script>
+
+    <!-- Optionally enable responsive features in IE8. Respond.js can be obtained from https://github.com/scottjehl/Respond -->
+    <script src="js/respond.min.js"></script>
+
+  </body>
+</html>
